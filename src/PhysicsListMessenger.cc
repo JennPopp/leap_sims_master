defc9da483636ffff34782e4711c22e438b98fe7
JennPopp <jennifer.popp@desy.de>
2021-04-26 17:20:45 +0200

    edit PhysListMess dir to /leap/phys

--- src/PhysicsListMessenger.cc
+++ src/PhysicsListMessenger.cc
@@ -42,14 +42,14 @@
 PhysicsListMessenger::PhysicsListMessenger(PhysicsList* pPhys)
 : G4UImessenger(),
   fPhysicsList(pPhys), fPhysDir(0), fListCmd(0)
-{ 
-  fPhysDir = new G4UIdirectory("/testem/phys/");
+{
+  fPhysDir = new G4UIdirectory("/leap/phys/");
   fPhysDir->SetGuidance("physics list commands");
 
-  fListCmd = new G4UIcmdWithAString("/testem/phys/addPhysics",this);  
+  fListCmd = new G4UIcmdWithAString("/leap/phys/addPhysics",this);  
   fListCmd->SetGuidance("Select standard/polarized physics list.");
   fListCmd->SetParameterName("PList",false);
-  fListCmd->AvailableForStates(G4State_PreInit);  
+  fListCmd->AvailableForStates(G4State_PreInit);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -64,7 +64,7 @@ PhysicsListMessenger::~PhysicsListMessenger()
 
 void PhysicsListMessenger::SetNewValue(G4UIcommand* command,
                                           G4String newValue)
-{       
+{
   if( command == fListCmd )
    { fPhysicsList->AddPhysicsList(newValue);}
 }
