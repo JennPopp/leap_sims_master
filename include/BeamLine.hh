9f2a158d82af9434cced348372852f5d0abca1f2
JennPopp <jennifer.popp@desy.de>
2024-01-29 16:16:32 +0100

    add BeamLine.hh

---
+++ include/BeamLine.hh
@@ -0,0 +1,24 @@
+// JP 29.01.2024
+// BeamLine.hh
+
+#ifndef BEAM_LINE_HH
+#define BEAM_LINE_HH
+
+//include necessary headers 
+#include "G4VUserDetectorConstruction.hh"
+#include "globals.hh"
+#include "Materials.hh"
+#include "G4ThreeVector.hh"
+
+class G4LogicalVolume;
+class G4Material;
+
+class BeamLine{
+public: 
+    BeamLine();
+    ~BeamLine();
+
+    // Add member functions to construct geometres for this subsystem
+    void ConstructBeamLine(G4LogicalVolume* LogicalWorld, G4double magthick); 
+};
+#endif
